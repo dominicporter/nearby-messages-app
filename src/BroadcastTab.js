37acfdfda5d2ff4e551d169a1b282c1a40b5ee16
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-08-08 19:59:02 +0100

    Set correct URL for broadcast

--- src/BroadcastTab.js
+++ src/BroadcastTab.js
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { API_URL } from './constants';
 
 const BroadcastTab = () => {
-  const [latitude, setLatitude] = useState("51.5007"); // Parliament Square latitude
-  const [longitude, setLongitude] = useState("-0.1246"); // Parliament Square longitude
-  const [message, setMessage] = useState("");
+  const [latitude, setLatitude] = useState('51.5007'); // Parliament Square latitude
+  const [longitude, setLongitude] = useState('-0.1246'); // Parliament Square longitude
+  const [message, setMessage] = useState('');
 
   const handleBroadcast = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/broadcast", { message, latitude, longitude });
+      await axios.post(`${API_URL}/broadcast`, { message, latitude, longitude });
       // Clear input fields after successful broadcast
-      setLatitude("");
-      setLongitude("");
-      setMessage("");
+      setLatitude('');
+      setLongitude('');
+      setMessage('');
     } catch (error) {
-      console.error("Error broadcasting message:", error);
+      console.error('Error broadcasting message:', error);
     }
   };
 
@@ -25,7 +26,7 @@ const BroadcastTab = () => {
         <div>
           <label>Latitude:</label>
           <input
-            type="text"
+            type='text'
             value={latitude}
             onChange={(e) => setLatitude(e.target.value)}
           />
@@ -33,7 +34,7 @@ const BroadcastTab = () => {
         <div>
           <label>Longitude:</label>
           <input
-            type="text"
+            type='text'
             value={longitude}
             onChange={(e) => setLongitude(e.target.value)}
           />
@@ -41,12 +42,12 @@ const BroadcastTab = () => {
         <div>
           <label>Message:</label>
           <input
-            type="text"
+            type='text'
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
         </div>
-        <button type="submit">Broadcast Message</button>
+        <button type='submit'>Broadcast Message</button>
       </form>
     </div>
   );
--- src/NearbyMessagesForm.js
+++ src/NearbyMessagesForm.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.REACT_APP_API_URL_PROD
-    : process.env.REACT_APP_API_URL_LOCAL;
+import { API_URL } from './constants';
 
 const NearbyMessagesForm = () => {
   const [latitude, setLatitude] = useState('51.5007'); // Parliament Square latitude
