4ccbdb39879d5a76ef788ed2b7e18b572e64da61
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-08-08 13:56:19 +0100

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Nearby Messages App/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  it('renders the header', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Nearby Messages App/i);
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it('displays nearby messages after form submission', async () => {
+    render(<App />);
+
+    // Mock the API response
+    const mockApiResponse = [
+      { message: 'Hello, nearby messages!' },
+      { message: 'Testing nearby messages.' },
+    ];
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockApiResponse),
+    });
+
+    // Fill out and submit the form
+    userEvent.type(screen.getByLabelText(/Latitude:/), '37.7749');
+    userEvent.type(screen.getByLabelText(/Longitude:/), '-122.4194');
+    userEvent.click(screen.getByRole('button', { name: /Search Nearby Messages/i }));
+
+    // Wait for messages to display
+    const message1 = await screen.findByText(/Hello, nearby messages!/i);
+    const message2 = await screen.findByText(/Testing nearby messages./i);
+
+    expect(message1).toBeInTheDocument();
+    expect(message2).toBeInTheDocument();
+  });
+
+  it('handles form submission error', async () => {
+    render(<App />);
+
+    // Mock the API response to simulate an error
+    global.fetch = jest.fn().mockRejectedValue(new Error('Network Error'));
+
+    // Fill out and submit the form
+    userEvent.type(screen.getByLabelText(/Latitude:/), '37.7749');
+    userEvent.type(screen.getByLabelText(/Longitude:/), '-122.4194');
+    userEvent.click(screen.getByRole('button', { name: /Search Nearby Messages/i }));
+
+    // Wait for error message to display
+    const errorMessage = await screen.findByText(/Error fetching nearby messages:/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
 });
